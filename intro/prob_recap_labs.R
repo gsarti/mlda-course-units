8d6e211fd0171fdfedf592f2fedf0c1241601ffa
gsarti <gabriele.sarti996@gmail.com>
2018-10-18 07:28:04 +0200

    Exercises and linear regression added

--- intro/prob_recap_labs.R
+++ intro/prob_recap_labs.R
@@ -119,3 +119,18 @@ ff <- 0.3 * dnorm(xx, 0) + 0.7 * dnorm(xx, 5)
 ### This is a mixture of normal distributions
 hist.scott(y, main = "", xlim = c(-4, 10))
 lines(xx, ff, col = "red", lwd = 2)
+
+# CLT exercise
+
+n <- c(5, 10, 50)
+p <- c(0.1,0.5,0.9)
+par(mfrow=c(3,3), pty = "s", pch=20)
+for(j in 1:3){
+  for (i in 1:3){
+    mean <- n[j]*p[i]
+    variance <- n[j]*p[i]*(1-p[i])
+    x <- seq(floor(mean - 4 * sqrt(variance)), floor(mean + 4 * sqrt(variance)), 1)
+    plot(x, dbinom(x, n[j], p[i]))
+    lines(dnorm(x, mean, sqrt(variance)), col=2, lwd=2)
+  }
+}
