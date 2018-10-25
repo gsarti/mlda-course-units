ea832e6bf0da1840229cceb848efd4599ac514fb
gsarti <gabriele.sarti996@gmail.com>
2018-10-25 09:31:55 +0200

    bias-variance tradeoff, cross validation course material added

--- linear_regression/linear_regression_labs.R
+++ linear_regression/linear_regression_labs.R
@@ -13,3 +13,19 @@ g1;g2
 
 par(mfrow=c(1,1), pty="s", pch=20)
 plot(galton$parent, galton$child, pch=20)
+ggplot(galton, aes(x = parent, y = child)) + geom_point()
+
+fit <- lm(child ~ parent, data = galton)
+predicted_df <- data.frame(child_pred=predict(fit, galton), parent = galton$parent)
+
+# Bubble chart with frequences
+ggplot(galton, aes(x = parent, y = child)) + 
+  geom_count() +
+  geom_line(color='red', data=predicted_df, aes(x = child_pred, y = parent))
+
+y <- galton$child
+x <- galton$parent
+beta1 <- cor(y,x) * sd(y) / sd(x)
+beta0 <- mean(y) - beta1 * mean(x)
+rbind(c(beta0, beta1), coef(lm(y ~ x)))
+cor(y,x)
